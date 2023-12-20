0cd6ebcd0bfecc8fd6c9a9849802e39185c4a35d
Kim DoHyeon <dohyun51731@gmail.com>
2023-12-20 17:37:08 +0900

    23_12_20 비만도 계산기

---
+++ lib/23_12_20/fat_calc/main.dart
@@ -0,0 +1,23 @@
+// 비만도 계산기
+import 'package:flutter/material.dart';
+import 'package:flutter_study/23_12_20/fat_calc/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
