87f171f1bd79f266ce46823254b505d25f775766
Kim DoHyeon <dohyun51731@gmail.com>
2023-12-20 22:14:20 +0900

    23_12_20 스톱워치(완성)

--- lib/23_12_20/stop_watch/stop_watch_screen.dart
+++ lib/23_12_20/stop_watch/stop_watch_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class StopWatchScreen extends StatefulWidget {
@@ -8,45 +9,84 @@ class StopWatchScreen extends StatefulWidget {
 }
 
 class _StopWatchScreenState extends State<StopWatchScreen> {
+  Timer? _timer;
+
+  int _time = 0;
+  bool _isRunning = false;
+
+  List<String> _lapTimes = [];
+
+  void _clickButton() {
+    _isRunning = !_isRunning;
+
+    if (_isRunning) {
+      _start();
+    } else {
+      _pause();
+    }
+  }
+
+  void _start() {
+    _timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
+
+  void _pause() {
+    _timer?.cancel();
+  }
+
+  void _reset() {
+    _isRunning = false;
+    _timer?.cancel();
+    _lapTimes.clear();
+    _time = 0;
+  }
+
+  void _recordLapTime(String time) {
+    _lapTimes.insert(0, '${_lapTimes.length + 1}등 $time');
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    int sec = _time ~/ 100;
+    String hundreds = '${_time % 100}'.padLeft(2, '0'); // 2자리로 표시를 하지만, 두자리가 아닐 경우에는 앞자리에 0을 넣는다
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('스톱워치'),
       ),
       body: Column(
         children: [
-          const SizedBox(height: 30,),
-          const Row(
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Text(
-                '0',
-                style: TextStyle(fontSize: 50),
+                '$sec',
+                style: const TextStyle(fontSize: 50),
               ),
               Text(
-                '00',
+                '$hundreds',
               ),
             ],
           ),
           SizedBox(
             width: 100,
-            height: 100,
+            height: 500,
             child: ListView(
-              children: [
-                Center(child: Text('111')),
-                Text('111'),
-                Text('111'),
-                Text('111'),
-                Text('111'),
-                Text('111'),
-                Text('111'),
-                Text('111'),
-                Text('111'),
-                Text('111'),
-                Text('111'),
-              ],
+              children: _lapTimes.map((time) => Center(child: Text(time))).toList(),
             ),
           ),
           const Spacer(),
@@ -55,21 +95,35 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
             children: [
               FloatingActionButton(
                 backgroundColor: Colors.blue,
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    _reset();
+                  });
+                },
                 child: const Icon(Icons.refresh),
               ),
               FloatingActionButton(
-                onPressed: () {},
-                child: const Icon(Icons.play_arrow),
+                onPressed: () {
+                  setState(() {
+                    _clickButton();
+                  });
+                },
+                child: _isRunning ? const Icon(Icons.pause) : const Icon(Icons.play_arrow),
               ),
               FloatingActionButton(
                 backgroundColor: Colors.green,
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    _recordLapTime('$sec.$hundreds');
+                  });
+                },
                 child: const Icon(Icons.add),
               ),
             ],
           ),
-          const SizedBox(height: 30,)
+          const SizedBox(
+            height: 30,
+          )
         ],
       ),
     );
