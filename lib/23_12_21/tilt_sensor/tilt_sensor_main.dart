e7b1cda376c63fef61ed462ff082b59db2c535ae
Kim DoHyeon <dohyun51731@gmail.com>
2023-12-21 16:45:29 +0900

    23_12_21 수평계

--- lib/23_12_21/tilt_sensor/tilt_sensor_main.dart
+++ lib/23_12_21/tilt_sensor/tilt_sensor_main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,6 +28,12 @@ class TiltSensor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 가로모드
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      // DeviceOrientation.landscapeRight,
+    ]);
+
 
     // 그림의 위치를 변경
     final centerX = MediaQuery.of(context).size.width / 2 - 50;
@@ -34,14 +42,33 @@ class TiltSensor extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned( // 그림의 위치를 변경하는 위젯
-            left: centerX, // 그림의 위치를 변경
-            top: centerY, // 그림의 위치를 변경
-            child: Container(
-              color: Colors.green,
-              width: 100,
-              height: 100,
-            ),
+          StreamBuilder<AccelerometerEvent>(
+            stream: accelerometerEventStream(),
+            builder: (context, snapshot) {
+              if(!snapshot.hasData) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              final event = snapshot.data!;
+              List<double> accelerometerValues = [event.x, event.y, event.z];
+              print(accelerometerValues);
+
+              return Positioned( // 그림의 위치를 변경하는 위젯
+                left: centerX + event.y * 20, // 그림의 위치를 변경
+                top: centerY + event.x * 20, // 그림의 위치를 변경
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                  ),
+                  // color: Colors.green, // BoxDecoration은 중복된 컬러를 제거해야 됨
+                  width: 100,
+                  height: 100,
+                ),
+              );
+            }
           ),
         ],
       ),
