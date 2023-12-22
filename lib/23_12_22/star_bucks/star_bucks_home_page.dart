431db9441b898f85687ffcad8526f69df0d0f78c
Kim DoHyeon <dohyun51731@gmail.com>
2023-12-22 12:11:53 +0900

    23_12_22 스타벅스 클론앱(슬리버 추가)

--- lib/23_12_22/star_bucks/star_bucks_home_page.dart
+++ lib/23_12_22/star_bucks/star_bucks_home_page.dart
@@ -6,17 +6,31 @@ class StarBucksHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('홈'),
-      ),
-      body: Center(
-        child: ListView(
-            children: [
-          Image.network(
-            'https://www.popco.net/zboard/data/dica_forum_fujifilm/2010/09/24/4314057764c9c811731d18.jpg',
-            fit: BoxFit.cover,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 150.0,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('홈'),
+              background: Image.network(
+                'https://www.popco.net/zboard/data/dica_forum_fujifilm/2010/09/24/4314057764c9c811731d18.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-        ]),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                // 다른 위젯 추가
+                Center(
+                  child: Image.network('https://www.popco.net/zboard/data/dica_forum_fujifilm/2010/09/24/4314057764c9c811731d18.jpg', fit: BoxFit.cover,),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
